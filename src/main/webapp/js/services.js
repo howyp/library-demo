a7bf0f5163ee905136e570a617a3c14b609cd5c7
nmoorcroft <nmoorcroft@gmail.com>
2013-05-15 15:40:10 +0100

    tests

--- src/main/webapp/js/services.js
+++ src/main/webapp/js/services.js
@@ -1,6 +1,6 @@
 var libraryServices = angular.module('libraryServices', [ 'ngResource' ]);
 
 libraryServices.factory('bookService', function($resource) {
-	return $resource('http://localhost:8080/angularjs-demo/api/books/:bookId', {}, {});
+	return $resource('api/books/:bookId', {}, {});
 });
 
--- src/test/js/unit/controllers.spec.js
+++ src/test/js/unit/controllers.spec.js
@@ -1,37 +1,61 @@
-describe("demo controllers", function() {
+describe("Controllers", function() {
+
+	describe('BookListCtrl', function() {
+
+		it('should get books from bookService', inject(function($rootScope, $controller) {
+
+			var scope = $rootScope.$new();
+			
+			var mockBookService = { query : jasmine.createSpy() };
+			
+			$controller(BookListCtrl, { $scope : scope, bookService : mockBookService });
+
+			expect(mockBookService.query).toHaveBeenCalled();
+			
+			expect(scope.search).toBeDefined();
+			
+		}));
+
+		it('should search books from bookService', inject(function($rootScope, $controller) {
+
+			var scope = $rootScope.$new();
+			
+			var mockBookService = { query : jasmine.createSpy() };
+			
+			$controller(BookListCtrl, { $scope : scope, bookService : mockBookService });
+
+			scope.search('query');
+			
+			expect(mockBookService.query).toHaveBeenCalledWith( { q : 'query' });
+			
+		}));
+	});
 
 	describe('LoginCtrl', function() {
 
-		it('should create login function', function() {
-			var scope = {}, http = {}, location = {};
-			
-			new LoginCtrl(scope, http, location);
-			
-			expect(scope.login).toBeDefined();
-			
-		});
-	});
-	
-	describe('BookListCtrl', function() {
-		
-		it('should create books', function() {
-			var scope = {};
-			var books = {};
-			var bookService = {
-				query: function() {
-					return books;
-				}
-			};
-			
-			new BookListCtrl(scope, bookService);
-			
-			expect(scope.books).toEqual(books);
-			
-		});
-		
-		
-	});
-	
-	
-});
+		it('should login via auth service', inject(function($httpBackend, $rootScope, $controller, $location) {
 
+			var scope = $rootScope.$new();
+
+			$controller(LoginCtrl, { $scope : scope });
+
+			var expected = { username : 'username', password : 'password' };
+			
+			$httpBackend.expectPOST('api/authenticate', expected).respond();
+
+			spyOn($location, 'path');
+			
+			scope.login({ username: 'username', password : 'password' });
+			$httpBackend.flush();
+
+			expect($location.path).toHaveBeenCalledWith('/books');
+
+			$httpBackend.verifyNoOutstandingExpectation();
+			$httpBackend.verifyNoOutstandingRequest();
+
+			
+		}));
+
+	});
+
+});
