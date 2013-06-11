7c398e82313a7ba5b8f4b85651a60c92978d54ba
Howard Perrin <howard@perrin.co.uk>
2013-06-11 17:13:18 +0100

    Tidy app.js

--- app.js
+++ app.js
@@ -15,16 +15,10 @@ app.listen(3000);
 console.log('Listening on port 3000');
 app.use(express.static(__dirname + '/src/main/webapp'))
 
-httpProxy.createServer(
-	// function (req, res, proxy) 
-	{
+httpProxy.createServer({
 	pathnameOnly: true,
 	router: {
 		'/api': '127.0.0.1:8080/', 
 		'/angular': '127.0.0.1:3000/' 
 	}
-	// proxy.proxyRequest(req, res, {
-	// 	host: '127.0.0.1',
-	// 	port: 3000
-	// });
 }).listen(80);
