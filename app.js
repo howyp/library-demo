15c5a92b20b364b5075e457f4f38945030977bb0
Mark <ham@zuehlke.com>
2013-06-13 12:17:49 +0100

    Tidied up tests, export the Express app from our module.

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 var http      = require('http');
 var express   = require('express');
 
-var app = express();
+app = express();
 
 app.get('/api/authenticate', function(req, res) {
 	res.send(200, {});
 });
 
 app.listen(3000);
-app.use('/angular', express.static(__dirname + '/src/main/webapp'))
+app.use('/angular', express.static(__dirname + '/src/main/webapp'));
 
+exports.app = app;
--- src/test/js/unit/app.spec.js
+++ src/test/js/unit/app.spec.js
@@ -1,18 +1,11 @@
 var supertest = require('supertest');
-var request = supertest('http://localhost:3000/api');
-
-resultHandler = function(done) {
-	return function(err, res) {
-		       		if (err) return done(err);
-		        	done();
-	}
-}
+var appUnderTest = require('./../../../../app.js');
+var request = supertest(appUnderTest.app);
 
 describe("The library demo api", function() {
 	it("should allow the user to authenticate", function(done) {
 		request.get('/authenticate')
 			   .expect('Content-Type', /json/)
-			   .expect(200)
-		       .end(resultHandler(done));
+			   .expect(200, done);
 	});
 });
