2d0842aecfa28bb604f2c281dce07ec47378ed73
Mark <ham@zuehlke.com>
2013-06-13 18:00:13 +0100

    Gruntfile runs angular tests as well.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,5 @@
 module.exports = function(grunt) {
 
-  debugger;
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     "jasmine-node": {
@@ -11,21 +10,36 @@ module.exports = function(grunt) {
     		forceexit: true
     	}
     },
-    // jasmine: {
-    // 	angular : {
-    // 		src: 'src/angular/js/**/*.js',
-    // 		options:  {
-    // 			specs: 'spec/angular/**/*.spec.js'
-    // 		}
-    // 	}
-    // }
+    jasmine: {
+    	angular : {
+    		src: 'src/angular/js/**/*.js',
+    		options:  {
+          vendor: [
+              /* source libraries */
+              "src/angular/js/lib/underscore-min.js",
+              "src/angular/js/lib/jquery-1.9.1.min.js",
+              "src/angular/js/lib/angular.js",
+              "src/angular/js/lib/angular-resource.min.js",
+              "src/angular/js/lib/angular-cookies.min.js",
+              "src/angular/js/lib/bootstrap.min.js",
+              "src/angular/js/lib/bootstrap-dropdown.js",
+              "src/angular/js/lib/base64.js"],
+            helpers: [
+              /* test dependencies */
+              "spec/angular/lib/angular-mocks.js",
+              "spec/angular/lib/mocks.js"
+          ],
+    			specs: 'spec/angular/**/*.spec.js'
+    		}
+    	}
+    }
   });
 
   grunt.loadNpmTasks('grunt-contrib-jasmine');
   grunt.loadNpmTasks('grunt-contrib-jasmine-node');
 
-  grunt.registerTask('test', ['jasmine_node']);
+  grunt.registerTask('test', ['jasmine-node', 'jasmine:angular']);
 
-  grunt.registerTask('default', ['jasmine_node']);
+  grunt.registerTask('default', ['test']);
 
 };
