c28eac34e7e7a9165ad48207a07d5c2b7473b1ae
Mark <ham@zuehlke.com>
2013-06-13 10:49:01 +0100

    Added spec that was missing from the last commit.

---
+++ src/test/js/unit/app.spec.js
@@ -0,0 +1,13 @@
+
+describe("The library demo api", function() {
+	it("should allow the user to authenticate", function(done) {
+
+		expect(true).toBeTruthy();
+		done();
+
+/*		request("http://localhost:3000/api/authenticate", function(error, response, body) {
+			expect(body).toEqual({id : 1, username : "neil", name : "neil moorcroft"});
+			done();
+			}); */
+	});
+});
